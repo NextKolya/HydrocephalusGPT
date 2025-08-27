1e197fc205af625794df697fcc3e4cab87c27f6b
NextKolya <phelixgluy@gmail.com>
2025-08-27 23:44:41 +0600

    Delete unused imports in Chat.tsx

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -1,19 +1,9 @@
-import { useEffect, useRef, ReactNode } from 'react';
 import { useParams } from 'react-router-dom';
-
-// @ts-ignore
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-// @ts-ignore
-import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 import useChatStore from '../stores/ChatStore';
 
-import { formatTime } from '../utils/formatTime';
-
 import MessageInput from './MessageInput';
 import ChatMessage from './ChatMessage';
-// @ts-ignore
-import AnswerLoading from './AnswerLoading';
 
 import chatStyles from './styles/Chat.module.css';
 
